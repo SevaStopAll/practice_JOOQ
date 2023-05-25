6120e73808536867ddf05985b41792e8b6727805
SevaStopAll <Sevastopallrh@yandex.ru>
2023-05-25 19:16:01 +0400

    init comment

---
+++ src/main/java/ru/sevastopall/practice_jooq/PracticeJooqApplication.java
@@ -0,0 +1,13 @@
+package ru.sevastopall.practice_jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJooqApplication.class, args);
+    }
+
+}
